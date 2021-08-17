72b129345256c8ccedfdbbf0890051b271e70588
Vimmi31 <viniciusmbarbosa2014@gmail.com>
2021-08-17 14:54:17 -0300

    TituloSubtituloAssuntoDeNoticia
    Imprime o Titulo, Subtítulo e o assunto da primeira noticia do site g1

--- requisicoes.py
+++ requisicoes.py
@@ -10,7 +10,15 @@ news = site.find('div', attrs={'class': 'feed-post-body'}) # "div" = tag que é 
 
 title = news.find('a', attrs={'class': 'feed-post-link gui-color-primary gui-color-hover'})
 
+subtitle = news.find('a', attrs={'class': 'gui-color-primary gui-color-hover feed-post-body-title bstn-relatedtext'})
+
+topic = news.find('span')
+
 print('\033[1;34m↓↓Código HTML da noticia↓↓\033[0;0m')
-print(f'{title.prettify()}\n') # Metodo que organiza o codigo html em algo mais legivel 
+print(title.prettify()) # Metodo que organiza o codigo html em algo mais legivel 
 print('\033[1;34m↓↓Titulo da noticia↓↓\033[0;0m')
 print(title.text) # Retorna o texto da tag "a"
+print('\033[1;34m↓↓Subtitulo da noticia↓↓\033[0;0m')
+print(subtitle.text)
+print('\033[1;34m↓↓Assunto da noticia↓↓\033[0;0m')
+print(topic.text)
